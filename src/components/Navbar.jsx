27d306bcb464e0d2143fa4f64fa29f0be278a8ef
whosnave <esardi10@gmail.com>
2024-09-01 20:39:04 +0700

    deskupd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import '../styles/navbar.css';
+import logo from '../assets/logo.png'; // Assuming your logo image is in the assets folder
 
 function Navbar() {
   return (
-    <>
     <nav className="navbar">
-      <a className='txnavbar' href="https://porsche.com">Porsche</a>
+      <div className="brand">
+        {/* <img src={logo} alt="Porsche Logo" className="logop" /> */}
+        <a className="txnavbar" href="https://porsche.com">Porsche</a>
+      </div>
       <ul className="navbar-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#models">Models</a></li>
@@ -12,7 +15,6 @@ function Navbar() {
         <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>
-    </>
   );
 }
 
